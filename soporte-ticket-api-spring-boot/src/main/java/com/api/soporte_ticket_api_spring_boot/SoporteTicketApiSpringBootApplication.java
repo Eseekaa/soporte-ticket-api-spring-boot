97515d321046a18afa3e22ab900e8c776682e53c
Matias Mercado <166638575+Eseekaa@users.noreply.github.com>
2025-06-06 02:48:02 -0400

    Add files via upload

---
+++ soporte-ticket-api-spring-boot/src/main/java/com/api/soporte_ticket_api_spring_boot/SoporteTicketApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.api.soporte_ticket_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoporteTicketApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoporteTicketApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ soporte-ticket-api-spring-boot/src/test/java/com/api/soporte_ticket_api_spring_boot/SoporteTicketApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.soporte_ticket_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoporteTicketApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
